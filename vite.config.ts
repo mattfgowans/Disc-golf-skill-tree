0fe492c5ea8a15f862cbeaab608393c570fc0ab7
Matthew Gowans <matthewgowans@216-macOS-MGowans.local>
2025-04-05 22:15:59 -0600

    Update vite config and add GitHub Actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/disc-golf-skill-tree/',
   server: {
     host: true, // This makes the server accessible on your local network
     port: 3000
